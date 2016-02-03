abfa03b35509d0409451bcc0a966c77898c1ba88
hang-wang <hwang12wou@gmail.com>
2016-02-03 10:53:45 -0500

    First pages commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Rise-of-the-Saiyans
+## This is a html game based on the story of Dragon Balls.
+
+**How to Run this Game**
+- Open index.html to play this game
+
+**How to Play this Game**
+
+- Try your best to collect more Dragon Balls
+- Reach the other end to earn more points!
+- You have 3 lives
+
+
+
+**Good Luck !**
---
+++ js/sounds.js
@@ -0,0 +1,35 @@
+
+var gameMusic = new Howl({
+	urls: ['sounds/music.mp3'], 
+	loop: true 
+});
+
+var gameOver = new Howl({ 
+	urls: ['sounds/gong.mp3']
+});
+
+var levelUp = new Howl({ 
+	urls: ['sounds/points.mp3'] 
+});
+
+var gemCollect = new Howl({ 
+	urls: ['sounds/gem.mp3'] 
+});
+
+var playerHit = new Howl({ 
+	urls: ['sounds/punch.mp3']
+});
+
+var finalboss = new Howl({
+	urls: ['sounds/finalboss.mp3']
+});
+
+var heroupgrade = new Howl({
+	urls: ['sounds/how.mp3']
+});
+
+var gameSelect = new Howl({ 
+	urls: ['sounds/blop.mp3']
+});
+
+												    							      
